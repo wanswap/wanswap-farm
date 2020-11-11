ffec75dd8dee05dca787211ff79b8a14f0e6cb4a
sharon-garcia <linaly.gs.gmail.com>
2020-11-11 18:39:34 +0800

    adjust waspPerBlock

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const WanSwapFarm = artifacts.require('WanSwapFarm.sol');
 const WaspToken = artifacts.require('WaspToken.sol');
 
 const devaddr = "0x4cf0a877e906dead748a41ae7da8c220e4247d9e";
-const waspPerBlock = "11932035127911420000"; //11.932035127911420000 wasp/block
+const waspPerBlock = "12000000000000000000"; // 12000000000000000000 wasp/block
 const startBlock = 1000;
 const testEndBlock = startBlock + 120960; // startBlock + 1.week
 const bonusEndBlock = testEndBlock + 1036800; // startBlock + 1.week + 2.month
