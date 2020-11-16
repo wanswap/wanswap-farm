b6b4d16e91a9eaec239ca07f4e16ffc9fcf62d83
sharon-garcia <linaly.gs.gmail.com>
2020-11-16 14:53:07 +0800

    update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 const WanSwapFarm = artifacts.require('WanSwapFarm.sol');
 const WaspToken = artifacts.require('WaspToken.sol');
 
-const devaddr = "0x4cf0a877e906dead748a41ae7da8c220e4247d9e";
+const devaddr = "".toLowerCase();
+const owner = "".toLowerCase();
 const waspPerBlock = "12000000000000000000"; // 12000000000000000000 wasp/block
-const startBlock = 1000;
+const startBlock = 10446000;
 const testEndBlock = startBlock + 120960; // startBlock + 1.week
 const bonusEndBlock = testEndBlock + 1036800; // startBlock + 1.week + 2.month
 const allEndBlock = bonusEndBlock + 11456640; // startBlock + 2.year
@@ -21,4 +22,14 @@ module.exports = async function (deployer, network, accounts) {
     allEndBlock
   );
   console.log('WanSwapFarm.address', WanSwapFarm.address);
+
+  let wasp = await WaspToken.deployed();
+  let farm = await WanSwapFarm.deployed();
+  let receipt = await wasp.transferOwnership(farm.address);
+  console.log("wasp owner", (await wasp.owner()))
+
+  if (owner !== accounts[0].toLowerCase()) {
+    receipt = await farm.transferOwnership(owner);
+  }
+  console.log("farm owner", (await farm.owner()))
 };
